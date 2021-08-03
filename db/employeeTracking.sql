2759de2d868806b38f527024ede484b3e5d8d7a2
Erik Stone <spologas@gmail.com>
2021-08-03 16:37:39 -0700

    added primary keys to link tables and added seeds for all the tables

--- db/employeeTracking.sql
+++ db/employeeTracking.sql
@@ -5,22 +5,27 @@ USE employee_DB;
 
 CREATE TABLE employee (
     id INT NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (employee_id),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL, 
     manager_id INT, 
-    FOREIGN KEY (manager_id)
+    FOREIGN KEY (manager_id),
+    role_id INT NOT NULL, REFERENCES role (role_id),
+    department_id INT NOT NULL, REFERENCES department (department_id)
 );
 
     CREATE TABLE role (
-        id INT NOT NULL, PRIMARY KEY (id),
+        id INT NOT NULL, PRIMARY KEY (role_id),
         title VARCHAR(30) NOT NULL,
         salary INT DECIMAL,
-        department_id INT NOT NULL
+        employee_id INT NOT NULL, REFERENCES employee (eployee_id),
+        department_id INT NOT NULL, REFERENCES department (department_id)
     );
 
     CREATE TABLE department (
-        id INT NOT NULL, PRIMARY KEY (id),
-        name VARCHAR(30) NOT NULL
+        id INT NOT NULL, PRIMARY KEY (department_id),
+        name VARCHAR(30) NOT NULL, 
+        employee_id INT NOT NULL, REFERENCES employee (eployee_id),
+        department_id INT NOT NULL, REFERENCES department (department_id)
     );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,14 @@
-INSERT INTO employee (first_name, last_name, role_id, id) 
-VALUES ('Mike', 'Diado', 05, 005),
-        ('Alice', 'Green', 04, 004),
-        ('Victor', 'John', 01, 001);
+INSERT INTO employee (first_name, last_name, role_id, manager_id, department, employee_id, department_id) 
+VALUES ('Mike', 'Diado', 05, "Engineer", 005, 15),
+        ('Alice', 'Green', 04, "Administrator", 004, 14),
+        ('Victor', 'John', 01, 0114, "Manager", 001, 11);
+
+INSERT INTO role (role_id, title, salary, employee_id, department_id)
+    values (05, "Engineer", 120000, 005, 15),
+            (04, "Administrator", 160000, 004, 14),
+            (01, "Manager", 250000, 001, 11);
+
+INSERT INTO department (department_id, name, employee_id)
+values  (15, "Engineering", 005),
+        (14, "Administration", 004),
+        (11, "Managment", 001);
