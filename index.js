94ec6408a454033f5d454720629d6f9deb029e09
Erik Stone <spologas@gmail.com>
2021-08-05 13:35:30 -0700

    added some text to index and DL'd console.table and eslint

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '345trehgf503(_)@m3H0lyFuck3r',
+      database: 'classlist_db'
+    },
+    console.log('connected to db!')
+);
 
 //view everything
 app.get('/employee_db', (req, res) => {
@@ -23,3 +36,16 @@ app.post('/employee_db', (req, res) => {
     })
 })
 
+const selection = {
+    async selectDepartment() { const department = await db.query('SELECT * FROM department')
+    }
+}
+
+
+
+//presented with all the options: view all departments, roles, employees, add a department, add an employee, update an employee
+//view departments (department id),roles (job title, role id, their department, and department id), employees (employee id, first and last name, job title, department, salaries, and manager)
+//prompted to add a department and does such
+//when selected add role, prompted to enter name, salary, and department for the role and then stored in the database
+//add employee, then prompted to enter first name, last name, role, and manager, which is then add to db
+//update an  employee, prompted to then select employee from stored database, then update their new role and then it is stored in database
